25d6ad780f0dae4830fb89a1c1b53a23aa8ec0ae
Kurikoko <zomahyon@gmail.com>
2021-09-08 16:44:13 +0900

    アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   
+  
+  
+  belongs_to :user
+  
+  #AcitveHashのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :shipping_fee
+    belongs_to :prefecture
+    belongs_to :scheduled_delivery
 end
