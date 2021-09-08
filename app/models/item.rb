63ad3a5d73fb024b7c7a0acfd4effd2a542e5066
Kurikoko <zomahyon@gmail.com>
2021-09-08 17:23:18 +0900

    Itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,30 @@
 class Item < ApplicationRecord
-  
-  
-  
   belongs_to :user
   
-  #AcitveHashのアソシエーション
+  # AcitveHashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
     belongs_to :status
     belongs_to :shipping_fee
     belongs_to :prefecture
     belongs_to :scheduled_delivery
+
+  # Validations
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
+  with_options presence: ture do
+    validates :name
+    validates :information
+    validates :price
+  end
+
+  validates :price, numericality: {in: 300..9999999, message: "is out of setting range"}, 
+    format: { with: /\d/, message: "is invalid. Input half-width caracters"}
+
 end
