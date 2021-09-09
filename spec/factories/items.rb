76c3404c402fc79b46985414b0b86d6c169a2093
Kurikoko <zomahyon@gmail.com>
2021-09-09 10:51:08 +0900

    Itemモデル単体テスト準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Types.rb_string(max_length: 40) }
+    information { Faker::Types.rb_string(max_length: 1000) }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    status_id { Faker::Number.between(from: 2, to: 7) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    shipping_fee_id { Faker::Number.between(from: 2, to: 3) }
+    scheduled_delivery_id { Faker::Number.between(from: 2, to: 4) }
+    price { Faker::Number.between(from: 300, to: 9999999) }
+
+    afeter(:build) do |item|
+      item.image.attach(io.File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
